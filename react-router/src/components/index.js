25dc60c5d10ea71ea0a15a8aff726dbab3caab8e
yash-parekh715 <parekhyash712005@gmail.com>
2024-06-10 19:16:26 +0530

    react-router partially complete

---
+++ react-router/src/components/index.js
@@ -0,0 +1,8 @@
+import Header from "./Header/Header";
+import Home from "./Home/Home";
+import Footer from "./Footer/Footer";
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+import User from "./User/User";
+
+export { Header, Home, Footer, About, Contact, User };
---
+++ react-router/src/main.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Home, About, Contact, User } from "./components/index.js";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromChildren,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//       {
+//         path: "contactUs",
+//         element: <Contact />,
+//       },
+//     ],
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />}></Route>
+      <Route path="about" element={<About />}></Route>
+      <Route path="contactUs" element={<Contact />}></Route>
+      <Route path="user/:id" element={<User />}></Route>
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
